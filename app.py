5988c1431c31cc7bc05055b0fbbd670ef0d466dc
Augusto <augusto@impa.br>
2016-04-13 23:22:25 -0300

    Implementing visual captcha

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     word = captcha.new_word()
-    captcha.gen_captcha(word[0], 'static/porkys.ttf', 25, 'static/' + word[1] + '.jpg', fmt='JPEG')
-    return word[0] + '<img src="static/' + word[1] + '.jpg" alt="Captcha" style="width:200px;height:30px;">'
+    captcha.gen_captcha(word[0], 'static/porkys.ttf', 25, 'static/captchas/' + word[1] + '.jpg', fmt='JPEG')
+    return word[0] + '<img src="static/captchas/' + word[1] + '.jpg" alt="Captcha" style="height:80px;">'
 
 @app.route('/data')
 def names():
