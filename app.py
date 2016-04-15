763c124f97ee1538913f587f4d785b8f3373ccd1
Augusto <augusto@impa.br>
2016-04-15 13:49:55 -0300

    Setting up view of feeds

--- app.py
+++ app.py
@@ -1,23 +1,42 @@
+import os
+import anydbm
 import captcha
+
+
+import logging
+logging.basicConfig(filename='log/output.log', filemode='w', level=logging.DEBUG)
 
 from flask import Flask, jsonify
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+
+#from bitcoinrpc.authproxy import AuthServiceProxy
 
 app = Flask(__name__)
 
 limiter = Limiter(
     app,
     key_func=get_remote_address,
-    global_limits=["2 per minute", "1 per second"],
+    global_limits=["20 per day", "1 per second"],
 )
 
 @app.route('/')
-@limiter.limit("2 per minute")
+@limiter.limit("20 per day")
 def index():
     word = captcha.new_word()
     captcha.gen_captcha(word[0], 'static/porkys.ttf', 25, 'static/captchas/' + word[1] + '.jpg', fmt='JPEG')
     return word[0] + '<img src="static/captchas/' + word[1] + '.jpg" alt="Captcha" style="height:80px;">'
+
+@app.route('/view')
+@limiter.limit("200 per day")
+def view():
+    db = anydbm.open(os.path.expanduser('data/feeds.db'), 'c')
+    outputlist = []
+    for key, value in db.iteritems():
+        temp = [key, value]
+        outputlist.append(temp)
+    db.close()
+    return jsonify(outputlist)
 
 @app.route('/data')
 def names():
