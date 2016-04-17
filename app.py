636b476966d176e8e7662cb5c18e6e2d7e56a8cd
Augusto <augusto@impa.br>
2016-04-17 11:48:16 -0300

    Removing avatar upload functionality (security)

--- app.py
+++ app.py
@@ -4,35 +4,19 @@ import string, datetime, usernames, validators
 import logging
 logging.basicConfig(filename='log/output.log', filemode='w', level=logging.DEBUG)
 
-from flask import Flask, jsonify, render_template, request, url_for, redirect, send_from_directory
+from flask import Flask, jsonify, render_template, request, url_for
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from random_words import RandomWords
-from werkzeug import secure_filename
 
 secret = 'this is not a secure secret'
 
-UPLOAD_FOLDER = os.path.dirname(os.path.realpath(__file__)) + '/avatars'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 3600
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-def listfiles(path):
-    try: lst = os.listdir(path)
-    except OSError:
-        return []
-    lst.remove('INFO.txt')
-    return lst
 
 limiter = Limiter(
     app,
     key_func=get_remote_address,
-    global_limits=["20 per day", "1 per second"],
+    global_limits=["200 per day", "1 per second"],
 )
 
 def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
@@ -55,20 +39,7 @@ def index():
     hash = hashlib.sha224(word + secret).hexdigest()
     filename = 'static/captchas/' + hash + '.jpg'
     captcha.gen_captcha(word, 'static/porkys.ttf', 25, filename, fmt='JPEG')
-    avatarlist = listfiles(UPLOAD_FOLDER)
-    avatarlist.append('DEFAULT')
-    return render_template('form.html', filename = filename, hash = hash, avatarlist = avatarlist)
-
-@app.route('/upload', methods=['GET', 'POST'])
-@limiter.limit("20 per day")
-def upload():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return render_template('message.html', message = 'File uploaded!')
-    return render_template('upload.html')
+    return render_template('form.html', filename = filename, hash = hash)
 
 @app.route('/submitted/', methods=['POST'])
 @limiter.limit("20 per day")
@@ -83,7 +54,6 @@ def submitted():
     feedurl = request.form['feedurl']
     #if not validators.url(feedurl):
     #    return render_template('error.html', message = 'This is not a valid URL!')
-    avatarname = request.form['avatarname']
     captcha = request.form['captcha']
     filename = 'static/captchas/' + hash + '.jpg'
     db = anydbm.open(os.path.expanduser('data/used.db'), 'c')
